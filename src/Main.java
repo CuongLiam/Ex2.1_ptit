2c8ddd7bee7368c0530d1f7533485a9021773172
CuongLiam <cuongdeptrai2706@gmail.com>
2025-04-22 22:50:13 +0700

    first commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(system.in);
+
+        System.out.println("Nhap a: ");
+        int a = scanner.nextInt();
+        System.out.println("Nhap b: ");
+        int b = scanner.nextInt();
+
+
+
+    }
+}
